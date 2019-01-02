b207dab2c90df5895a0780e8a69f513e77b4470c
yeongtaek, Hong <mozodev@users.noreply.github.com>
2019-01-03 00:02:38 +0900

    메뉴 정리, 문의하기, 블로그, 초기 화면, 국영문 페이지를 추가합니다.

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,38 @@
 ---
+layout: null
 ---
 
 $(function(){
-  $("#rss-feeds").rss("{{ site.naver_blog }}", {
-    limit: 5,
-    layoutTemplate: '<div class="row">{entries}</div>',
-    entryTemplate: '<div class="col-md-6 mb-5"><h3><a href="{url}" target="_blank">[{author}@{date}]{title}</a></h3><p>{shortBodyPlain}</p></div>',
-  }, function callback(res){
-    console.log(res);
-  });
+  if ($("#rss-feeds").length) {
+    $("#rss-feeds").rss("{{ site.data.blog.blog_rss }}", {
+      limit: 1000,
+      layoutTemplate: '<div class="row">{entries}</div>',
+      entryTemplate: '<div class="item"><h3><a href="{url}" target="_blank">{title}</a></h3><p>[{author}@{date}]</p><p>{bodyPlain}</p></div>',
+      dateFormat: 'YYYYMMDD',
+      success: function(){
+        console.log($('#rss-feeds .item').length);
+        $('#pagination-container').pagination({
+          dataSource: $('#rss-feeds .item').toArray(),
+          pageSize: 5,
+          callback: function(data, pagination) {
+            console.log(data);
+            var html = simpleTemplating(data);
+            $('#rss-feeds').html(html);
+            var w = $('.paginationjs-pages ul').width();
+            $('.paginationjs-pages').addClass('mx-auto').width(w);
+          }
+        })
+      }
+    });
+  }
 });
+
+function simpleTemplating(data) {
+  var html = '<ul class="list-unstyled">';
+  $.each(data, function(index, item){
+    console.log(item);
+    html += '<li class="mb-5">'+ item.outerHTML +'</li>';
+  });
+  html += '</ul>';
+  return html;
+}
